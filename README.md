77d92fe8cb4ce8e933597b8346f3195e03bc2ea6
evnoj <mail@evnoj.net>
2026-06-03 11:24:37 -0600

    simplify script and improve speed

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
-A lua script to map crow voltage values to Morphagene pitches. Load a saw wave onto Morphagene, plugin the output into your audio interface to monitor pitch and waveshape, plug a crow output into the varispeed CV input, then run the script and it will create a Lua table mapping voltage values to musical notes. This then enables crow to control the pitch of Morphagene to hit in-tune equal temperament relationships to the pitch of the source material
+A Lua script to map voltage values to [Morphagene](https://www.makenoisemusic.com/modules/morphagene/) pitches. Intended for use with with the [monome crow](https://monome.org/docs/crow/) to enable crow to play Morphagene in tune. Load a saw wave onto Morphagene, plugin the output into your audio interface to monitor pitch and waveshape, plug a crow output into the varispeed CV input, then run the script and it will create a Lua table mapping voltage values to musical notes. This then enables crow to control the pitch of Morphagene to hit in-tune equal temperament relationships to the pitch of the source material
 
-dependencies:
+> to use this script with a different CV output module, change the `set_cv()` function in the script
+
+# dependencies
 - [ChucK](https://chuck.stanford.edu/release/)
     - the `chuck` command must be available on your `PATH`
     - the default ChucK plugins ("chugins") must be installed, these are installed with the ChucK download linked above but does not come with all ChucK installations, ex. homebrew has a ChucK formula but does not come with the chugins
 
 # instructions
-Load `c4-saw-falling-10s.wav` onto your Morphagene, and load the reel.
+1. Clone this repo and `cd` into it.
+2. Load `c4-saw-falling-10s.wav` onto your Morphagene, and load the reel.
+3. Turn **varispeed**, **gene size**, and **slide** fully ccw
+4. Turn the varispeed CV attenuverter fully clockwise
+5. Turn **morph** to ~9 o clock spot for seamless looping (right varispeed indicator should be yellow)
+6. Plug the desired crow output into the varispeed CV input
+  - If not using output 1, be sure to change `crow_output` at the top of the script
+7. Get the output of morphagene into your audio interface, and ensure that the interface is sending only the signal from Morphagene to your computer (i.e. other inputs are muted or unplugged).
+8. Run the script like `lua morphagene-crow-mapper.lua`
+  - The script takes ~20 minutes to run. While the script is running, don't plug or unplug audio devices into your computer.
+  - There are some errors that can occur while the script is running, and if so, it either prompts for input or drops into an interactive debugger so that you can resolve the issue. The debugger is [debugger.lua](https://codeberg.org/slembcke/debugger.lua), enter `h` at the debugger for a list of commands, you'll probably want to also use:
+    - `t` to view the stack trace and what frame you're in
+    - `u` and `d` to move up/down frames
+    - `l` to view local variables
+    - `e` to execute code in the current frame
+      - ex. `e cv=0` sets the `cv` variable, which is the control voltage that will be sent from crow's output
+9. When the script completes, it writes the table to `morphagene-note-cv-map.lua` in the directory that the script was run from.
 
-plug the desired crow output into the varispeed CV input, turn the varispeed knob fully counterclockwise and the attunuverter fully clockwise, then run the script like `lua morphagene-crow-mapper.lua`
+# LLM disclosure
+I used significant LLM assistance to write the ChucK code that does the audio analysis. I wanted the audio analysis to be simple and able to be done via shell commands, so I had an LLM write minimal PoCs for whatever audio tools it could think of. ChucK appeared the simplest, and I used manual edits and an LLM to iterate on it.
+
+I wrote all the Lua.
--- morphagene-crow-mapper.lua
+++ morphagene-crow-mapper.lua
@@ -1,16 +1,45 @@
 local dbg = require("debugger")
 
--- chuck script that analyzes frequency and "direction" of the saw wave
--- direction is -1 for backwards (rising saw), 1 for forwards (falling saw), 0 for stopped
--- prints to stdout a single line like:
--- freq,note,direction
+-- CONFIGURATION VARIABLES
+-- the midi notes to find CV for, negative means reverse playback, 0 means stopped
+-- I couldn't get pitch detection to work with chuck for midi note less than ~35
+local notes = {-84, -83, -82, -81, -80, -79, -78, -77, -76, -75, -74, -73, -72, -71, -70, -69, -68, -67, -66, -65, -64, -63, -62, -61, -60, -59, -58, -57, -56, -55, -54, -53, -52, -51, -50, -49, -48, -47, -46, -45, -44, -43, -42, -41, -40, -39, -38, -37, 0, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84}
+
+-- the crow output
+local crow_output = 1
+
+-- UTILITIES
+function sign(n)
+    if n < 0 then
+        return -1
+    elseif n > 0 then
+        return 1
+    else
+        return 0
+    end
+end
+
+-- IO FUNCTIONS
+function set_cv(v)
+    os.execute("druid exec 'output["..crow_output.."].volts = "..v.."'")
+end
+
+-- chuck script that analyzes frequency (as midi note #) and "direction" of the saw wave
+-- prints the note to stdout
+-- negative means that it is reversed (rising saw wave)
+-- 0 means silence (morphagene is stopped)
+-- 666 means it couldn't identify the wave as either rising or falling
+-- 1500 or -1500 means it couldn't identify a pitch
 local chuck_script = [[
 adc => Gain g => blackhole;
-
+adc => PitchTrack pitch => blackhole;
+pitch.overlap(5);
+pitch.frame(4096);
 0 => int rising;
 0 => int falling;
 0.0 => float sumSq;
 0 => int sampCount;
+0.003 => float silenceThresh; // ~-50 dBFS
 
 fun void sampleLoop() {
     0.0 => float prev;
@@ -26,30 +55,21 @@ fun void sampleLoop() {
 }
 spork ~ sampleLoop();
 
-0.003 => float silenceThresh; // ~-50 dBFS
-
-// re-init each time to avoid reporting prev pitch 
-adc => PitchTrack pitch => blackhole;
-pitch.overlap(5);
-pitch.frame(4096);
-
 250::ms => now;
 
 Math.sqrt(sumSq / sampCount) => float rms;
-pitch.get() => float freq;
 
-chout <= freq <= ",";
 // if unable to detect pitch, ftom reports -1500
-chout <= Std.ftom(freq) <= ",";
+Std.ftom(pitch.get()) => float note;
 
 if (rms < silenceThresh) {
     chout <= 0;
 } else if (rising > falling * 2) {
-    chout <= -1;
+    chout <= -1 * note;
 } else if (falling > rising * 2) {
-    chout <= 1;
+    chout <= note;
 } else {
-    chout <= "symmetric/other";        
+    chout <= 666;
 }
 
 chout <= IO.newline();
@@ -58,235 +78,15 @@ chout <= IO.newline();
 0.0 => sumSq; 0 => sampCount;
 ]]
 
---- Save a table to disk.
--- Saves tables, numbers, booleans and strings.
--- Inside table references are saved.
--- Does not save userdata, metatables, functions and indices of these.
--- Based on http://lua-users.org/wiki/SaveTableToFile by ChillCode.
--- @tparam table tbl Table to save.
--- @tparam string filename Location to save to.
--- @return On failure, returns an error msg.
-function tab_save(tbl, filename)
-  local charS, charE = "   ", "\n"
-  local file, err = io.open(filename, "wb")
-  if err then return err end
+-- returns the midi note number
+function chuck_run(adc, rate)
+    local note =  io.popen("echo '"..chuck_script.."' | chuck --adc:"..adc.." --srate:"..rate.." /dev/stdin"):read()
 
-  -- initiate variables for save procedure
-  local tables, lookup = { tbl }, { [tbl] = 1 }
-  file:write("return {"..charE)
-
-  for idx, t in ipairs(tables) do
-    file:write("-- Table: {"..idx.."}"..charE)
-    file:write("{"..charE)
-    local thandled = {}
-
-    for i, v in ipairs(t) do
-      thandled[i] = true
-      local stype = type(v)
-      -- only handle value
-      if stype == "table" then
-        if not lookup[v] then
-          table.insert(tables, v)
-          lookup[v] = #tables
-        end
-        file:write(charS.."{"..lookup[v].."},"..charE)
-      elseif stype == "string" then
-        file:write(charS..string.format("%q", v)..","..charE)
-      elseif stype == "number" then
-        file:write(charS..tostring(v)..","..charE)
-      elseif stype == "boolean" then
-        file:write(charS..tostring(v)..","..charE)
-      end
-    end
-
-    for i, v in pairs(t) do
-      -- escape handled values
-      if (not thandled[i]) then
-
-        local str = ""
-        local stype = type(i)
-        -- handle index
-        if stype == "table" then
-          if not lookup[i] then
-             table.insert(tables, i)
-             lookup[i] = #tables
-          end
-          str = charS.."[{"..lookup[i].."}]="
-        elseif stype == "string" then
-          str = charS.."["..string.format("%q", i).."]="
-        elseif stype == "number" then
-          str = charS.."["..tostring(i).."]="
-        elseif stype == "boolean" then
-          str = charS.."["..tostring(i).."]="
-        end
-
-        if str ~= "" then
-          stype = type(v)
-          -- handle value
-          if stype == "table" then
-            if not lookup[v] then
-              table.insert(tables, v)
-              lookup[v] = #tables
-            end
-            file:write(str.."{"..lookup[v].."},"..charE)
-          elseif stype == "string" then
-            file:write(str..string.format("%q", v)..","..charE)
-          elseif stype == "number" then
-            file:write(str..tostring(v)..","..charE)
-          elseif stype == "boolean" then
-            file:write(str..tostring(v)..","..charE)
-          end
-        end
-      end
-    end
-    file:write("},"..charE)
-  end
-  file:write("}")
-  file:close()
-end
-
-condition_set = {
-    {note=84, dir=-1},
-    {note=83, dir=-1},
-    {note=82, dir=-1},
-    {note=81, dir=-1},
-    {note=80, dir=-1},
-    {note=79, dir=-1},
-    {note=78, dir=-1},
-    {note=77, dir=-1},
-    {note=76, dir=-1},
-    {note=75, dir=-1},
-    {note=74, dir=-1},
-    {note=73, dir=-1},
-    {note=72, dir=-1},
-    {note=71, dir=-1},
-    {note=70, dir=-1},
-    {note=69, dir=-1},
-    {note=68, dir=-1},
-    {note=67, dir=-1},
-    {note=66, dir=-1},
-    {note=65, dir=-1},
-    {note=64, dir=-1},
-    {note=63, dir=-1},
-    {note=62, dir=-1},
-    {note=61, dir=-1},
-    {note=60, dir=-1},
-    {note=59, dir=-1},
-    {note=58, dir=-1},
-    {note=57, dir=-1},
-    {note=56, dir=-1},
-    {note=55, dir=-1},
-    {note=54, dir=-1},
-    {note=53, dir=-1},
-    {note=52, dir=-1},
-    {note=51, dir=-1},
-    {note=50, dir=-1},
-    {note=49, dir=-1},
-    {note=48, dir=-1},
-    {note=47, dir=-1},
-    {note=46, dir=-1},
-    {note=45, dir=-1},
-    {note=44, dir=-1},
-    {note=43, dir=-1},
-    {note=42, dir=-1},
-    {note=41, dir=-1},
-    {note=40, dir=-1},
-    {note=39, dir=-1},
-    {note=38, dir=-1},
-    {note=37, dir=-1},
-    {dir=0},
-    {note=37, dir=1},
-    {note=38, dir=1},
-    {note=39, dir=1},
-    {note=40, dir=1},
-    {note=41, dir=1},
-    {note=42, dir=1},
-    {note=43, dir=1},
-    {note=44, dir=1},
-    {note=45, dir=1},
-    {note=46, dir=1},
-    {note=47, dir=1},
-    {note=48, dir=1},
-    {note=49, dir=1},
-    {note=50, dir=1},
-    {note=51, dir=1},
-    {note=52, dir=1},
-    {note=53, dir=1},
-    {note=54, dir=1},
-    {note=55, dir=1},
-    {note=56, dir=1},
-    {note=57, dir=1},
-    {note=58, dir=1},
-    {note=59, dir=1},
-    {note=60, dir=1},
-    {note=61, dir=1},
-    {note=62, dir=1},
-    {note=63, dir=1},
-    {note=64, dir=1},
-    {note=65, dir=1},
-    {note=66, dir=1},
-    {note=67, dir=1},
-    {note=68, dir=1},
-    {note=69, dir=1},
-    {note=70, dir=1},
-    {note=71, dir=1},
-    {note=72, dir=1},
-    {note=73, dir=1},
-    {note=74, dir=1},
-    {note=75, dir=1},
-    {note=76, dir=1},
-    {note=77, dir=1},
-    {note=78, dir=1},
-    {note=79, dir=1},
-    {note=80, dir=1},
-    {note=81, dir=1},
-    {note=82, dir=1},
-    {note=83, dir=1},
-    {note=84, dir=1},
-}
-
-local condition_checkers = {
-    note = function(desired, measured)
-        local diff_abs = math.abs(desired - measured)
-        local tolerance = 0.02
-
-        if diff_abs < tolerance then
-            return true
-        else
-            return false
-        end
-    end,
-    freq = function(desired, measured)
-        local diff_abs = math.abs(desired - measured)
-        local tolerance = 0.02
-
-        if diff_abs < tolerance then
-            return true
-        else
-            return false
-        end
-    end,
-    dir = function(desired, measured)
-        if desired == measured then
-            return true
-        else
-            return false
-        end
-    end,
-}
-
-function chuck_run(script, adc, rate)
-    local s =  io.popen("echo '"..script.."' | chuck --adc:"..adc.." --srate:"..rate.." /dev/stdin"):read()
-    if not s then
+    if not note then
         dbg()
     end
 
-    local freq,note,dir = string.match(s, "(.+),(.+),(.+)")
-    return {
-        freq = tonumber(freq),
-        note = tonumber(note),
-        dir = tonumber(dir),
-    }
+    return tonumber(note)
 end
 
 -- use this to check what the chuck script is returning while in the debugger
@@ -294,71 +94,104 @@ function chuck_check()
     print(io.popen("echo '"..chuck_script.."' | chuck --adc:"..adc_index.." --srate:"..srate.." /dev/stdin"):read())
 end
 
+-- set global adc_index and sample rate for the adc chuck uses
+function chuck_adc_setup()
+    local chuck_adcs = io.popen("chuck --probe", "r")
+    for line in chuck_adcs:lines() do
+        print(line)
+    end
+
+    print("enter the index of the ADC that the morphagene signal is coming in on:")
+    adc_index = io.read("*n")
+
+    print('enter the sample rate of the device (press "d" for default 48000):')
+    srate = io.read("*n")
+
+    if not srate then
+        srate = 48000
+    end
+end
+
 -- INIT
+local note_cv_map = {}
 
-local chuck_adcs = io.popen("chuck --probe", "r")
-for line in chuck_adcs:lines() do
-    print(line)
-end
-
-print("enter the index of the ADC that the morphagene signal is coming in on:")
-local adc_index = io.read("*n")
-
-print('enter the sample rate of the device (press "d" for default 48000):')
-local srate = io.read("*n")
-
-if not srate then
-    srate = 48000
-end
-
-local cv = 0
+chuck_adc_setup()
 
 local pass_threshold = 3
-local step_size = 0.01
-local step_direction_swap_threshold = 20
-local absolute_step_threshold = 100
-for i,conditions in ipairs(condition_set) do
+local step_threshold = 20
+local tolerance = 0.02 -- 2 cents
+
+cv = 0
+
+for i,note_target in ipairs(notes) do
     local step_direction = 1
     local steps = 0
     local passes = 0
+    local step_size = 0.02
 
-    ::check_condition::
-    os.execute("druid exec 'output[1].volts = "..cv.."'")
-    local measured = chuck_run(chuck_script, adc_index, srate)
-    local pass = true
-    for condition,desired in pairs(conditions) do
-        pass = pass and condition_checkers[condition](desired, measured[condition])
+    ::check_note::
+    set_cv(cv)
+
+    -- if adcs/dacs are added/removed while script runs (which can happen by accident w/ bluetooth or dumb macOS stuff) then this can fail, in which case user must set adc again
+    local note_measured = nil
+    while true do
+        note_measured = chuck_run(adc_index, srate)
+
+        if not note_measured then
+            print("error while running chuck script, set adc index again")
+            chuck_adc_setup()
+        else
+            break
+        end
     end
-    if not pass then
-        steps = steps + 1
-        if steps > absolute_step_threshold then
-            print("steps exceeded threshold on condition "..i)
-            print("cv was "..cv)
-            print("entering debugger, please set cv manually then continue")
-            dbg()
-        elseif steps > step_direction_swap_threshold then
-            step_direction = step_direction * -1
-        end
 
-        print("condition "..i.." failed with cv "..cv.." on step "..steps)
-        if conditions.note then
-            print("desired note: "..conditions.note)
-            print("measured note: "..measured.note)
-        end
+    local err = note_target - note_measured
 
-        cv = cv + (step_size * step_direction)
-        goto check_condition
-    else
+    if math.abs(err) < tolerance then -- pass
         passes = passes + 1
         print("condition "..i.." pass count "..passes)
 
         if passes == pass_threshold then
             print('passed '..i)
-            conditions.volts = cv
-        else
-            goto check_condition
+            table.insert(note_cv_map, {note = note_target, cv= cv})
+        else -- check again
+            goto check_note
         end
+    else -- fail
+        if sign(err) ~= step_direction then -- overshot
+            step_size = step_size / 2
+            step_direction = step_direction * -1
+
+        end
+
+        steps = steps + 1
+        cv = cv + (step_size * step_direction)
+        print("note "..note_target.." failed with cv "..cv.." on step "..steps)
+        print("measured note: "..note_measured)
+
+        if steps > step_threshold then
+            print("steps exceeded threshold on note "..note_target)
+            print("entering debugger, please set cv manually then continue")
+            dbg()
+        end
+
+        goto check_note
     end
 end
 
-tab_save(condition_set, 'morphagene-crow-map.lua')
+-- save the table
+local indent, eol = "    ", "\n"
+local file, err = io.open('morphagene-note-cv-map.lua', "wb")
+if err then
+    print("error while opening file, entering debugger")
+    dbg()
+end
+
+file:write("{"..eol)
+
+for _,t in ipairs(note_cv_map) do
+    file:write(indent.."["..t.note.."]="..t.cv..","..eol)
+end
+
+file:write("}"..eol)
+file:close()
