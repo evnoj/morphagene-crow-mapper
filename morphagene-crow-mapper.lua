96227d407e006c2f1df45ced6b9cc47c5dca5b31
evnoj <mail@evnoj.net>
2026-06-03 09:04:20 -0600

    script functional, but error-prone and slow

--- morphagene-crow-mapper.lua
+++ morphagene-crow-mapper.lua
@@ -1,3 +1,9 @@
+local dbg = require("debugger")
+
+-- chuck script that analyzes frequency and "direction" of the saw wave
+-- direction is -1 for backwards (rising saw), 1 for forwards (falling saw), 0 for stopped
+-- prints to stdout a single line like:
+-- freq,note,direction
 local chuck_script = [[
 adc => Gain g => blackhole;
 
@@ -36,19 +42,14 @@ chout <= freq <= ",";
 // if unable to detect pitch, ftom reports -1500
 chout <= Std.ftom(freq) <= ",";
 
-if (rising > falling * 2) {
-    chout <= "rising";
+if (rms < silenceThresh) {
+    chout <= 0;
+} else if (rising > falling * 2) {
+    chout <= -1;
 } else if (falling > rising * 2) {
-    chout <= "falling";
+    chout <= 1;
 } else {
     chout <= "symmetric/other";        
-}
-chout <= ",";
-
-if (rms < silenceThresh) {
-    chout <= "silence";
-} else {
-    chout <= "signal";
 }
 
 chout <= IO.newline();
@@ -57,110 +58,240 @@ chout <= IO.newline();
 0.0 => sumSq; 0 => sampCount;
 ]]
 
-local conditions = {
-    {note=84, direction=rising},
-    {note=83, direction=rising},
-    {note=82, direction=rising},
-    {note=81, direction=rising},
-    {note=80, direction=rising},
-    {note=79, direction=rising},
-    {note=78, direction=rising},
-    {note=77, direction=rising},
-    {note=76, direction=rising},
-    {note=75, direction=rising},
-    {note=74, direction=rising},
-    {note=73, direction=rising},
-    {note=72, direction=rising},
-    {note=71, direction=rising},
-    {note=70, direction=rising},
-    {note=69, direction=rising},
-    {note=68, direction=rising},
-    {note=67, direction=rising},
-    {note=66, direction=rising},
-    {note=65, direction=rising},
-    {note=64, direction=rising},
-    {note=63, direction=rising},
-    {note=62, direction=rising},
-    {note=61, direction=rising},
-    {note=60, direction=rising},
-    {note=59, direction=rising},
-    {note=58, direction=rising},
-    {note=57, direction=rising},
-    {note=56, direction=rising},
-    {note=55, direction=rising},
-    {note=54, direction=rising},
-    {note=53, direction=rising},
-    {note=52, direction=rising},
-    {note=51, direction=rising},
-    {note=50, direction=rising},
-    {note=49, direction=rising},
-    {note=48, direction=rising},
-    {note=47, direction=rising},
-    {note=46, direction=rising},
-    {note=45, direction=rising},
-    {note=44, direction=rising},
-    {note=43, direction=rising},
-    {note=42, direction=rising},
-    {note=41, direction=rising},
-    {note=40, direction=rising},
-    {note=39, direction=rising},
-    {note=38, direction=rising},
-    {note=37, direction=rising},
-    {signal='silence'},
-    {note=37, direction=falling},
-    {note=38, direction=falling},
-    {note=39, direction=falling},
-    {note=40, direction=falling},
-    {note=41, direction=falling},
-    {note=42, direction=falling},
-    {note=43, direction=falling},
-    {note=44, direction=falling},
-    {note=45, direction=falling},
-    {note=46, direction=falling},
-    {note=47, direction=falling},
-    {note=48, direction=falling},
-    {note=49, direction=falling},
-    {note=50, direction=falling},
-    {note=51, direction=falling},
-    {note=52, direction=falling},
-    {note=53, direction=falling},
-    {note=54, direction=falling},
-    {note=55, direction=falling},
-    {note=56, direction=falling},
-    {note=57, direction=falling},
-    {note=58, direction=falling},
-    {note=59, direction=falling},
-    {note=60, direction=falling},
-    {note=61, direction=falling},
-    {note=62, direction=falling},
-    {note=63, direction=falling},
-    {note=64, direction=falling},
-    {note=65, direction=falling},
-    {note=66, direction=falling},
-    {note=67, direction=falling},
-    {note=68, direction=falling},
-    {note=69, direction=falling},
-    {note=70, direction=falling},
-    {note=71, direction=falling},
-    {note=72, direction=falling},
-    {note=73, direction=falling},
-    {note=74, direction=falling},
-    {note=75, direction=falling},
-    {note=76, direction=falling},
-    {note=77, direction=falling},
-    {note=78, direction=falling},
-    {note=79, direction=falling},
-    {note=80, direction=falling},
-    {note=81, direction=falling},
-    {note=82, direction=falling},
-    {note=83, direction=falling},
-    {note=84, direction=falling},
+--- Save a table to disk.
+-- Saves tables, numbers, booleans and strings.
+-- Inside table references are saved.
+-- Does not save userdata, metatables, functions and indices of these.
+-- Based on http://lua-users.org/wiki/SaveTableToFile by ChillCode.
+-- @tparam table tbl Table to save.
+-- @tparam string filename Location to save to.
+-- @return On failure, returns an error msg.
+function tab_save(tbl, filename)
+  local charS, charE = "   ", "\n"
+  local file, err = io.open(filename, "wb")
+  if err then return err end
+
+  -- initiate variables for save procedure
+  local tables, lookup = { tbl }, { [tbl] = 1 }
+  file:write("return {"..charE)
+
+  for idx, t in ipairs(tables) do
+    file:write("-- Table: {"..idx.."}"..charE)
+    file:write("{"..charE)
+    local thandled = {}
+
+    for i, v in ipairs(t) do
+      thandled[i] = true
+      local stype = type(v)
+      -- only handle value
+      if stype == "table" then
+        if not lookup[v] then
+          table.insert(tables, v)
+          lookup[v] = #tables
+        end
+        file:write(charS.."{"..lookup[v].."},"..charE)
+      elseif stype == "string" then
+        file:write(charS..string.format("%q", v)..","..charE)
+      elseif stype == "number" then
+        file:write(charS..tostring(v)..","..charE)
+      elseif stype == "boolean" then
+        file:write(charS..tostring(v)..","..charE)
+      end
+    end
+
+    for i, v in pairs(t) do
+      -- escape handled values
+      if (not thandled[i]) then
+
+        local str = ""
+        local stype = type(i)
+        -- handle index
+        if stype == "table" then
+          if not lookup[i] then
+             table.insert(tables, i)
+             lookup[i] = #tables
+          end
+          str = charS.."[{"..lookup[i].."}]="
+        elseif stype == "string" then
+          str = charS.."["..string.format("%q", i).."]="
+        elseif stype == "number" then
+          str = charS.."["..tostring(i).."]="
+        elseif stype == "boolean" then
+          str = charS.."["..tostring(i).."]="
+        end
+
+        if str ~= "" then
+          stype = type(v)
+          -- handle value
+          if stype == "table" then
+            if not lookup[v] then
+              table.insert(tables, v)
+              lookup[v] = #tables
+            end
+            file:write(str.."{"..lookup[v].."},"..charE)
+          elseif stype == "string" then
+            file:write(str..string.format("%q", v)..","..charE)
+          elseif stype == "number" then
+            file:write(str..tostring(v)..","..charE)
+          elseif stype == "boolean" then
+            file:write(str..tostring(v)..","..charE)
+          end
+        end
+      end
+    end
+    file:write("},"..charE)
+  end
+  file:write("}")
+  file:close()
+end
+
+condition_set = {
+    {note=84, dir=-1},
+    {note=83, dir=-1},
+    {note=82, dir=-1},
+    {note=81, dir=-1},
+    {note=80, dir=-1},
+    {note=79, dir=-1},
+    {note=78, dir=-1},
+    {note=77, dir=-1},
+    {note=76, dir=-1},
+    {note=75, dir=-1},
+    {note=74, dir=-1},
+    {note=73, dir=-1},
+    {note=72, dir=-1},
+    {note=71, dir=-1},
+    {note=70, dir=-1},
+    {note=69, dir=-1},
+    {note=68, dir=-1},
+    {note=67, dir=-1},
+    {note=66, dir=-1},
+    {note=65, dir=-1},
+    {note=64, dir=-1},
+    {note=63, dir=-1},
+    {note=62, dir=-1},
+    {note=61, dir=-1},
+    {note=60, dir=-1},
+    {note=59, dir=-1},
+    {note=58, dir=-1},
+    {note=57, dir=-1},
+    {note=56, dir=-1},
+    {note=55, dir=-1},
+    {note=54, dir=-1},
+    {note=53, dir=-1},
+    {note=52, dir=-1},
+    {note=51, dir=-1},
+    {note=50, dir=-1},
+    {note=49, dir=-1},
+    {note=48, dir=-1},
+    {note=47, dir=-1},
+    {note=46, dir=-1},
+    {note=45, dir=-1},
+    {note=44, dir=-1},
+    {note=43, dir=-1},
+    {note=42, dir=-1},
+    {note=41, dir=-1},
+    {note=40, dir=-1},
+    {note=39, dir=-1},
+    {note=38, dir=-1},
+    {note=37, dir=-1},
+    {dir=0},
+    {note=37, dir=1},
+    {note=38, dir=1},
+    {note=39, dir=1},
+    {note=40, dir=1},
+    {note=41, dir=1},
+    {note=42, dir=1},
+    {note=43, dir=1},
+    {note=44, dir=1},
+    {note=45, dir=1},
+    {note=46, dir=1},
+    {note=47, dir=1},
+    {note=48, dir=1},
+    {note=49, dir=1},
+    {note=50, dir=1},
+    {note=51, dir=1},
+    {note=52, dir=1},
+    {note=53, dir=1},
+    {note=54, dir=1},
+    {note=55, dir=1},
+    {note=56, dir=1},
+    {note=57, dir=1},
+    {note=58, dir=1},
+    {note=59, dir=1},
+    {note=60, dir=1},
+    {note=61, dir=1},
+    {note=62, dir=1},
+    {note=63, dir=1},
+    {note=64, dir=1},
+    {note=65, dir=1},
+    {note=66, dir=1},
+    {note=67, dir=1},
+    {note=68, dir=1},
+    {note=69, dir=1},
+    {note=70, dir=1},
+    {note=71, dir=1},
+    {note=72, dir=1},
+    {note=73, dir=1},
+    {note=74, dir=1},
+    {note=75, dir=1},
+    {note=76, dir=1},
+    {note=77, dir=1},
+    {note=78, dir=1},
+    {note=79, dir=1},
+    {note=80, dir=1},
+    {note=81, dir=1},
+    {note=82, dir=1},
+    {note=83, dir=1},
+    {note=84, dir=1},
+}
+
+local condition_checkers = {
+    note = function(desired, measured)
+        local diff_abs = math.abs(desired - measured)
+        local tolerance = 0.02
+
+        if diff_abs < tolerance then
+            return true
+        else
+            return false
+        end
+    end,
+    freq = function(desired, measured)
+        local diff_abs = math.abs(desired - measured)
+        local tolerance = 0.02
+
+        if diff_abs < tolerance then
+            return true
+        else
+            return false
+        end
+    end,
+    dir = function(desired, measured)
+        if desired == measured then
+            return true
+        else
+            return false
+        end
+    end,
 }
 
 function chuck_run(script, adc, rate)
     local s =  io.popen("echo '"..script.."' | chuck --adc:"..adc.." --srate:"..rate.." /dev/stdin"):read()
-    local freq,note,dir,sig = string.match(s, "(.+),(.+),(.+),(.+)")
-    return tonumber(freq),tonumber(note),dir,sig
+    if not s then
+        dbg()
+    end
+
+    local freq,note,dir = string.match(s, "(.+),(.+),(.+)")
+    return {
+        freq = tonumber(freq),
+        note = tonumber(note),
+        dir = tonumber(dir),
+    }
+end
+
+-- use this to check what the chuck script is returning while in the debugger
+function chuck_check()
+    print(io.popen("echo '"..chuck_script.."' | chuck --adc:"..adc_index.." --srate:"..srate.." /dev/stdin"):read())
 end
 
 -- INIT
@@ -182,7 +313,52 @@ end
 
 local cv = 0
 
-for _,cond in ipairs(conditions) do
-    -- check condition
-    local freq,note,dir,sig = chuck_run(chuck_script, adc_index, srate)
+local pass_threshold = 3
+local step_size = 0.01
+local step_direction_swap_threshold = 20
+local absolute_step_threshold = 100
+for i,conditions in ipairs(condition_set) do
+    local step_direction = 1
+    local steps = 0
+    local passes = 0
+
+    ::check_condition::
+    os.execute("druid exec 'output[1].volts = "..cv.."'")
+    local measured = chuck_run(chuck_script, adc_index, srate)
+    local pass = true
+    for condition,desired in pairs(conditions) do
+        pass = pass and condition_checkers[condition](desired, measured[condition])
+    end
+    if not pass then
+        steps = steps + 1
+        if steps > absolute_step_threshold then
+            print("steps exceeded threshold on condition "..i)
+            print("cv was "..cv)
+            print("entering debugger, please set cv manually then continue")
+            dbg()
+        elseif steps > step_direction_swap_threshold then
+            step_direction = step_direction * -1
+        end
+
+        print("condition "..i.." failed with cv "..cv.." on step "..steps)
+        if conditions.note then
+            print("desired note: "..conditions.note)
+            print("measured note: "..measured.note)
+        end
+
+        cv = cv + (step_size * step_direction)
+        goto check_condition
+    else
+        passes = passes + 1
+        print("condition "..i.." pass count "..passes)
+
+        if passes == pass_threshold then
+            print('passed '..i)
+            conditions.volts = cv
+        else
+            goto check_condition
+        end
+    end
 end
+
+tab_save(condition_set, 'morphagene-crow-map.lua')
